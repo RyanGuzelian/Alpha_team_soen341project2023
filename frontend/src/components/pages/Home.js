91db5b78291caf74ffeece80028ac0fce6c8ac94
Mansi Gairola <mansigairola28800@gmail.com>
2023-02-28 17:55:46 -0500

    changes to navbar and home, material ui included

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../navbar/navbar'
+import Header from '../navbar/Header'
+
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <h1>Home page</h1>
+        </>
+    )
+}
+
+export default Home
