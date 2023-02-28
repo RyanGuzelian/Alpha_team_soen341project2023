534fa1dec88c4f67a88b9bdb51876810352c2585
Mansi Gairola <mansigairola28800@gmail.com>
2023-02-28 17:56:43 -0500

    changes to navbar and home

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,67 +1,177 @@
-import React, {useState} from 'react'
-import{BiSearch} from 'react-icons/bi'
-import{BsPerson} from 'react-icons/bs'
-import{HiOutlineMenuAlt4} from 'react-icons/hi'
-import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import WorkIcon from '@mui/icons-material/Work';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@emotion/react';
 
-import './navbarStyles.css'
+const pages = ['Home', 'Log In'];
+
 
 function Navbar() {
-    const[nav,setNav] = useState(false)
-    const handleNav=()=>setNav(!nav)
-return (
-    
+    const { palette } = useTheme();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-        <div className={nav? 'navbar navbar-bg' : 'navbar'}>
-            <div classname="logo">
-                <h2>HIRED</h2>
-            </div>
-            <ul className="nav-menu">
-                <li>Home</li>
-                <li>Employer</li>
-                <li>Post Jobs</li>
-                <li>Search Jobs</li>
-            </ul>
-            <div className="nav-icons">
-                <BiSearch className='icon' style={{marginRight: '1rem'}} />
-                <BsPerson className='icon' />
-            </div>
-            <div className="hamburger" onClick={handleNav}>
-                <HiOutlineMenuAlt4 className='icon' />
-            </div>
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-            <div className={nav? 'mobile-menu active' : 'mobile-menu'}>
-                <ul className="mobile-nav">
-            
-                <li>Home</li>
-                <li>Employer</li>
-                <li>Post Jobs</li>
-                <li>Search Jobs</li>
-                
-                </ul>
-                <div className = "mobile-menu-bottom">
-                    <div className="menu-icons">
-                        <button>Search</button>
-                        <button>Account</button>
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-                    </div>
-                    <div className="social-icons">
-                    <FaFacebook className='icon' />
-                        <FaInstagram className='icon' />
-                        <FaTwitter className='icon' />
-                        <FaPinterest className='icon' />
-                        <FaYoutube className='icon' />
-
-
-                    </div>
-                </div>
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
 
 
-            </div>
+    return (
+        <AppBar position="static">
+            <Container >
+                {/* principal Menu */}
+                <Toolbar disableGutters>
+                    <WorkIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        JOB PORTAL
+                    </Typography>
 
-        </div>
-)
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <WorkIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.2rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        JOB PORTAL
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {/* menu desktop */}
+
+                        <Button
+                            onClick={handleCloseNavMenu}
+                            sx={{ my: 2, color: 'white', display: 'block' }}>
+                            <Link to="/" style={{ color: 'white', textDecoration: "none" }}>
+                                Home
+                            </Link>
+                        </Button>
+
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+
+                            <MenuItem onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center"><Link style={{ textDecoration: "none", color: palette.primary.main }} to="/admin/dashboard">Dashboard</Link></Typography>
+                            </MenuItem>
+
+                            <MenuItem onClick={handleCloseUserMenu}>
+                                <Typography textAlign="center"><Link style={{ textDecoration: "none", color: palette.primary.main }} to="/login">Log In</Link></Typography>
+                            </MenuItem>
+
+                            <MenuItem >
+                                <Typography style={{ textDecoration: "none", color: palette.primary.main }} textAlign="center">Log Out</Typography>
+                            </MenuItem>
+
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
-
-export default Navbar
+export default Navbar;
