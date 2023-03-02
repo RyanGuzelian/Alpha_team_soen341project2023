cbf6413f53bacbc1239a96ce7b133e878ceb68f1
RyanGuzelian <ryanguzelian@gmail.com>
2023-03-02 00:58:39 -0500

    Readded some packages to package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import EditProfile from './components/pages/EditProfile';
 
-import Navbar from './components/fixed/Navbar'
+import Navbar from './components/fixed/navbar'
 import Profile from './components/pages/Profile';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
